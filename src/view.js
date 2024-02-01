8662daa0b11ae51853be5bc00f2cbbc2f3a836a6
mariahikari <mariahikariyps@gmail.com>
2024-02-01 14:52:39 -0300

    atualização view.js

--- src/view.js
+++ src/view.js
@@ -1,8 +1,21 @@
 export const renderItems = (data) => {
-  console.log(data) 
+  console.log(data)
   const cards = document.createElement('ul');
   cards.classList.add('container');
   // Aquí comienza tu código y puedes retornar lo que tu necesites
-  return cards;
+
+  data.forEach((item) => {
+    cards.innerHTML +=`
+    <dl itemscope itemtype="autorasEmFoco">
+      <dt><img src="${item.imageUrl}" alt="Capa do Livro" /></dt>
+      <dd itemprop="livro">"${item.livro}"</dd>
+      <dd itemprop="autora">"${item.autora}"</dd>
+      <dd itemprop="numeroDePaginas">${item.numeroDePaginas}"</dd>
+      <dd itemprop="preco">${item.preçoMedio}"</dd>
+      <button class='sinopseButton'><dt>Sinopse</dt><dd itemprop="Sinopse"></dd></button>
+    </dl>
+    `;
+  })
+return cards;
 };
 
