57fe7d9893da74ea475d7a6cbc5506cde3079f3b
mariahikari <mariahikariyps@gmail.com>
2024-02-01 17:01:46 -0300

    inclusão de summary no view.js

--- src/view.js
+++ src/view.js
@@ -5,16 +5,26 @@ export const renderItems = (data) => {
   // Aquí comienza tu código y puedes retornar lo que tu necesites
 
   data.forEach((item) => {
-    cards.innerHTML +=`
+    cards.innerHTML += `
     <dl itemscope itemtype="autorasEmFoco">
       <dt><img src=${item.imageUrl} alt="Capa do Livro" /></dt>
       <dd itemprop="livro" class="livro_name">${item.livro}</dd>
       <dd itemprop="autora">${item.autora}</dd>
+      <dd itemprop="numeroDePaginas">Gênero: ${item.detalhes.genero}</dd>
       <dd itemprop="numeroDePaginas">Páginas: ${item.detalhes.numeroDePaginas}</dd>
       <dd itemprop="preco">Preço: ${item.detalhes.preçoMedio}</dd>
-      <button class='sinopseButton'><dd itemprop="sinopse">"Sinopse"</dd></button>
+      <details open>
+      <summary><strong>Ver +</strong></summary>
+      <ul>
+      <li>Sobre o livro: ${item.sobreOLivro}</li>
+      <li>Sinopse: ${item.sinopse}</li>
+      <li>Prêmios: ${item.detalhes.premios}</li>
+      <li>Adaptações: ${item.detalhes.adaptações}</li>
+      </ul>
+      </details>
+
     </dl>
     `;
   })
-return cards;
+  return cards;
 };
