f663f5926ac5404c8ac9fd757528c6b7b432a732
mariahikari <mariahikariyps@gmail.com>
2024-02-01 15:28:21 -0300

    exclusão das aspas - <dd>

--- src/view.js
+++ src/view.js
@@ -7,12 +7,12 @@ export const renderItems = (data) => {
   data.forEach((item) => {
     cards.innerHTML +=`
     <dl itemscope itemtype="autorasEmFoco">
-      <dt><img src="${item.imageUrl}" alt="Capa do Livro" /></dt>
-      <dd itemprop="livro">"${item.livro}"</dd>
-      <dd itemprop="autora">"${item.autora}"</dd>
-      <dd itemprop="numeroDePaginas">${item.numeroDePaginas}"</dd>
-      <dd itemprop="preco">${item.preçoMedio}"</dd>
-      <button class='sinopseButton'><dt>Sinopse</dt><dd itemprop="Sinopse"></dd></button>
+      <dt><img src=${item.imageUrl} alt="Capa do Livro" /></dt>
+      <dd itemprop="livro">${item.livro}</dd>
+      <dd itemprop="autora">${item.autora}</dd>
+      <dd itemprop="numeroDePaginas">${item.numeroDePaginas}</dd>
+      <dd itemprop="preco">${item.preçoMedio}</dd>
+      <button class='sinopseButton'><dd itemprop="sinopse">"Sinopse"</dd></button>
     </dl>
     `;
   })
