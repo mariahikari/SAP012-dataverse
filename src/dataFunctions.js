9a18790f9b27ead04b9734fbd3d5c0dc4d0df749
mariahikari <mariahikariyps@gmail.com>
2024-02-05 13:05:36 -0300

    inclusão no datafunctions.js e main.js

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,9 +1,14 @@
 // Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
 
-export const example = () => {
-  return 'example';
+export const filterData = (data, filterBy, value) => {
+  const filtro1 = data.filter(item => {
+    if ('filtroGenero' in item && filterBy in item.filtroGenero)
+    return item.filtroGenero[filterBy] === value
+  });
+  
+  return filtro1;
 };
 
-export const anotherExample = () => {
+export const sortData = (data, sortBy, sortOrder) => {
   return [];
 };
--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
-//import { example } from './dataFunctions.js';
+import { filterData } from './dataFunctions.js';
 import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
 
 //console.log(example, renderItems(data), data);
 
+const filtroGenero = document.querySelector('#filtroGenero');
+const filtroPreco = document.querySelector('#filtroPreco');
+const ordenarPor = document.querySelector('#ordenarPor');
+const resetButton = document.querySelector('#resetButton');
 const cardsLivros = document.querySelector('#root');
 cardsLivros.appendChild(renderItems(data));
+
+
+filtroGenero.addEventListener('change', () => {
+    const generoSelecionado = filtroGenero.value;
+    const dadosFiltrados = filterData(data, 'genero', generoSelecionado);
+    cardsLivros.innerHTML = '';
+    cardsLivros.appendChild(renderItems(dadosFiltrados));
+  });
+
