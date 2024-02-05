e1a39a2c3dc5e01f44a7763863d60d009811b1c0
Marcele <marcele2608@gmail.com>
2024-02-05 13:21:03 -0300

    alterações dataFunctions e main

--- src/main.js
+++ src/main.js
@@ -15,9 +15,14 @@ const cardsLivros = document.querySelector('#root');
 cardsLivros.appendChild(renderItems(data));
 
 // Event listener for the genre filter
+
 filtroGenero.addEventListener('change', () => {
   const generoSelecionado = filtroGenero.value;
+  console.log('Selected Genre:', generoSelecionado);
+  
   const dadosFiltrados = filterData(data, 'genero', generoSelecionado);
+  console.log('Filtered Data:', dadosFiltrados);
+  
   cardsLivros.innerHTML = '';
   cardsLivros.appendChild(renderItems(dadosFiltrados));
 });
