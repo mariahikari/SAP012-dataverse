27c095dc78abe6fa215f2d30ccdd48ba4f87a8ac
mariahikari <mariahikariyps@gmail.com>
2024-02-11 11:20:22 -0300

    ajuste no HTML (data-testid escrito em inglês, conforme readme) e criação de  testes unitários

--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -1,18 +1,31 @@
-import { example, anotherExample } from '../src/dataFunctions.js';
+import { filterData, sortData } from '../src/dataFunctions.js';
 import { data as fakeData } from './data.js';
 
-console.log(fakeData);
+// console.log(fakeData);
 
-describe('example', () => {
+describe('filterData', () => {
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('returns `livrosDeFicção`', () => {
+    const filteredData = filterData(fakeData, 'genero', 'Ficção');
+    expect(filteredData.length).toBe(4);
   });
+
 });
 
-describe('anotherExample', () => {
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+describe('sortData', () => {
+
+  it('returns `ordemAscendente`', () => {
+    const sortedData = sortData(fakeData, 'livro', 'asc')
+    expect(sortedData[0].livro).toBe('A Poderosa Chefona');
+    expect(sortedData[1].livro).toBe('Bolsa Blindada');
+    expect(sortedData[2].livro).toBe('Cinquenta Tons de Cinza');
+  });
+
+  it('returns `ordemDescendente`', () => {
+    const sortedData = sortData(fakeData, 'livro', 'desc')
+    expect(sortedData[0].livro).toBe('Sempre Vivemos no Castelo');
+    expect(sortedData[1].livro).toBe('Sejamos Todos Feministas');
+    expect(sortedData[2].livro).toBe('Quarto de Despejo: Diário de uma Favelada');
   });
 });
