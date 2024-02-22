fbf85542084151a649f095d2d0ab2f7bf672a837
Marcele <marcele2608@gmail.com>
2024-02-22 12:06:28 -0300

    map adicionado ao computeStats

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -25,7 +25,7 @@ export const sortData = (data, sortBy, sortOrder) => {
 };
 
 export const computeStats = (data) => {
-  const count = data.reduce((accumulator) => accumulator + 1, 0);
+  const count = data.map(item => item.genero).reduce((accumulator) => accumulator + 1, 0);
 
   return count;
 };
