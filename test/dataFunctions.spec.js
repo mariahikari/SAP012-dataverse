8ebd779e5c235277d5756079e3aee3b91210e206
Marcele <marcele2608@gmail.com>
2024-02-23 18:28:05 -0300

    teste computeStats

--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -1,4 +1,4 @@
-import { filterData, sortData } from '../src/dataFunctions.js';
+import { filterData, sortData, computeStats } from '../src/dataFunctions.js';
 import { data as fakeData } from './data.js';
 
 // console.log(fakeData);
@@ -6,7 +6,7 @@ import { data as fakeData } from './data.js';
 describe('filterData', () => {
 
   it('returns `livrosDeFicção`', () => {
-    const filteredData = filterData(fakeData, 'genero', 'Ficção');
+    const filteredData = filterData(fakeData, 'generoLivro', 'Ficção');
     expect(filteredData.length).toBe(4);
   });
 
@@ -16,16 +16,32 @@ describe('filterData', () => {
 describe('sortData', () => {
 
   it('returns `ordemAscendente`', () => {
-    const sortedData = sortData(fakeData, 'livro', 'asc')
-    expect(sortedData[0].livro).toBe('A Poderosa Chefona');
-    expect(sortedData[1].livro).toBe('Bolsa Blindada');
-    expect(sortedData[2].livro).toBe('Cinquenta Tons de Cinza');
+    const sortedData = sortData(fakeData, 'name', 'asc')
+    expect(sortedData[0].name).toBe('A Poderosa Chefona');
+    expect(sortedData[1].name).toBe('Bolsa Blindada');
+    expect(sortedData[2].name).toBe('Cinquenta Tons de Cinza');
   });
 
   it('returns `ordemDescendente`', () => {
-    const sortedData = sortData(fakeData, 'livro', 'desc')
-    expect(sortedData[0].livro).toBe('Sempre Vivemos no Castelo');
-    expect(sortedData[1].livro).toBe('Sejamos Todos Feministas');
-    expect(sortedData[2].livro).toBe('Quarto de Despejo: Diário de uma Favelada');
+    const sortedData = sortData(fakeData, 'name', 'desc')
+    expect(sortedData[0].name).toBe('Sempre Vivemos no Castelo');
+    expect(sortedData[1].name).toBe('Sejamos Todos Feministas');
+    expect(sortedData[2].name).toBe('Quarto de Despejo: Diário de uma Favelada');
+  });
+});
+
+
+describe('computeStats', () => {
+  it('returns `quantidade de livros`', () => {
+    const data = [
+      { generoLivro: 'Ficção' },
+      { generoLivro: 'Ficção' },
+      { generoLivro: 'Ficção' },
+    ];
+
+    const expectedCount = 3;
+    const result = computeStats(data);
+
+    expect(result).toBe(expectedCount);
   });
 });
